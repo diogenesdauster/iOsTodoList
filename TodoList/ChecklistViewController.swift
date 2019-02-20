19d85182fc995763650422bb23f63c84f7a35a9c
Diógenes Dauster <diogenes.dauster@gmail.com>
2019-02-19 23:03:56 -0300

    refactor: add some methods to tableviewcontroller

--- TodoList/ChecklistViewController.swift
+++ TodoList/ChecklistViewController.swift
@@ -14,7 +14,16 @@ class ChecklistViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for:  indexPath)
+        return cell
+    }
 
 }
 
