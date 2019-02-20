5996ea6fb80a5f9ec1d26fcd7d17ecf19b897bbc
Diógenes Dauster <diogenes.dauster@gmail.com>
2019-02-19 23:15:40 -0300

    feat: customize a tablecell

--- TodoList/ChecklistViewController.swift
+++ TodoList/ChecklistViewController.swift
@@ -22,6 +22,15 @@ class ChecklistViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for:  indexPath)
+        
+        if let label = cell.viewWithTag(1000) as? UILabel {
+            if indexPath.row == 0 {
+                label.text = "Run 5km"
+            }else{
+                label.text = "Only Sleep"
+            }
+        }
+        
         return cell
     }
 
