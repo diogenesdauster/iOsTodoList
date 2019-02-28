a7e2fcffa5328685e2d1bec2e444d93d86d45ce4
Diógenes Dauster <diogenes.dauster@gmail.com>
2019-02-28 18:31:37 -0300

    feat: add NavigationButton and IBAction add button add todo in the navigation controller and create IBAction for handling the action when it recives click

--- TodoList/ChecklistViewController.swift
+++ TodoList/ChecklistViewController.swift
@@ -65,6 +65,13 @@ class ChecklistViewController: UITableViewController {
         item.toogle()
     
     }
-
+    
+    
+    @IBAction func addTodo(){
+        let alert = UIAlertController(title: "New Todo", message: "added item", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
