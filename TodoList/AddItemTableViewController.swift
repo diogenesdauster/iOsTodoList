7badb6076fec95717149ac697fb33265b067beb1
Diógenes Dauster <diogenes.dauster@gmail.com>
2019-03-05 16:49:03 -0300

    feat: customize the table view for static row in the static row add a textField for receiving data and configurate the profile manual for build in my own phone.

--- TodoList/AddItemTableViewController.swift
+++ TodoList/AddItemTableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class AddItemTableViewController: UITableViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.largeTitleDisplayMode = .never
@@ -22,6 +24,14 @@ class AddItemTableViewController: UITableViewController {
     
     @IBAction func done(_ sender: Any) {
         navigationController?.popViewController(animated: true)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        textField.becomeFirstResponder()
+    }
+    
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
     }
     
 }
